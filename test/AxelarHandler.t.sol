d1c2c858b8d7e5da3123d7f26a7ce9dd2487ab67
0xAmarok <0xAmarok@pm.me>
2023-10-09 04:37:35 +0200

    Project setup

---
+++ test/AxelarHandler.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import {AxelarHandler} from "src/AxelarHandler.sol";
+
+contract AxelarHandlerTest is Test {
+    function setUp() public {}
+}
