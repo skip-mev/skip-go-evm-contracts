586e36b46f8bfd1371d4d9867ac14a8a914acd8e
0xAmarok <0xAmarok@pm.me>
2023-10-09 04:38:45 +0200

    Simplified WETH interface

---
+++ src/interfaces/IWETH.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2015, 2016, 2017 Dapphub
+// Adapted by Ethereum Community 2021
+pragma solidity ^0.8.0;
+
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+/// @dev Wrapped Ether v10 (WETH10) is an Ether (ETH) ERC-20 wrapper. You can `deposit` ETH and obtain a WETH10 balance which can then be operated as an ERC-20 token. You can
+/// `withdraw` ETH from WETH10, which will then burn WETH10 token in your wallet. The amount of WETH10 token in any wallet is always identical to the
+/// balance of ETH deposited minus the ETH withdrawn with that specific wallet.
+interface IWETH is IERC20 {
+    /// @dev `msg.value` of ETH sent to this contract grants caller account a matching increase in WETH10 token balance.
+    /// Emits {Transfer} event to reflect WETH10 token mint of `msg.value` from `address(0)` to caller account.
+    function deposit() external payable;
+
+    /// @dev Burn `value` WETH10 token from caller account and withdraw matching ETH to the same.
+    /// Emits {Transfer} event to reflect WETH10 token burn of `value` to `address(0)` from caller account.
+    /// Requirements:
+    ///   - caller account must have at least `value` balance of WETH10 token.
+    function withdraw(uint256 value) external;
+
+    /// @dev Burn `value` WETH10 token from caller account and withdraw matching ETH to account (`to`).
+    /// Emits {Transfer} event to reflect WETH10 token burn of `value` to `address(0)` from caller account.
+    /// Requirements:
+    ///   - caller account must have at least `value` balance of WETH10 token.
+    function withdrawTo(address payable to, uint256 value) external;
+}
