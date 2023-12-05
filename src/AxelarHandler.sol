707b515e8c674369d00f89bb93c6df48626c5647
0xAmarok <0xAmarok@pm.me>
2023-12-05 18:53:05 +0100

    WETH unsupported chains modifications

--- src/AxelarHandler.sol
+++ src/AxelarHandler.sol
@@ -31,6 +31,10 @@ contract AxelarHandler is
     error ZeroNativeSent();
     error NonNativeCannotBeUnwrapped();
     error NativePaymentFailed();
+    error WrappingNotEnabled();
+
+    bytes public constant DISABLED_SYMBOL =
+        keccak256(abi.encodePacked("DISABLED"));
 
     bytes32 private _wETHSymbolHash;
 
@@ -67,6 +71,7 @@ contract AxelarHandler is
         string memory destinationChain,
         string memory destinationAddress
     ) external payable {
+        if (_wETHSymbolHash == DISABLED_SYMBOL) revert WrappingNotEnabled();
         if (msg.value == 0) revert ZeroNativeSent();
 
         // Get the token address from the gateway.
@@ -119,6 +124,7 @@ contract AxelarHandler is
         uint256 amount, // argument passed to both child contract calls
         uint256 gasPaymentAmount
     ) external payable {
+        if (_wETHSymbolHash == DISABLED_SYMBOL) revert WrappingNotEnabled();
         if (amount == 0) revert ZeroAmount();
         if (gasPaymentAmount == 0) revert ZeroGasAmount();
         if (msg.value != amount + gasPaymentAmount)
@@ -300,6 +306,7 @@ contract AxelarHandler is
         // If unwrap is set and the token can be unwrapped.
         if (
             unwrap &&
+            _wETHSymbolHash != DISABLED_SYMBOL
             keccak256(abi.encodePacked(tokenSymbol)) == _wETHSymbolHash
         ) {
             // Unwrap native token.
