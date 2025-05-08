12d3d45d36cbcb531578ded51942327be87a1e9a
thal0x <thal0x.eth@gmail.com>
2025-05-08 09:52:36 -0400

    add tests for failure case

--- SwapRouter/test/SkipGoSwapRouter.t.sol
+++ SwapRouter/test/SkipGoSwapRouter.t.sol
@@ -86,6 +86,55 @@ contract SkipGoSwapRouterTest is Test {
         assertEq(amountOut, expectedAmountOut);
     }
 
+    function test_revertSwapExactIn_WhenAmountOutIsLessThanAmountIn() public {
+        address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        address wbtc = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+
+        SkipGoSwapRouter.Hop[] memory hops = new SkipGoSwapRouter.Hop[](2);
+
+        {
+            UniswapV2Adapter.UniswapV2Data memory hopOneData = UniswapV2Adapter.UniswapV2Data({
+                pool: 0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc,
+                zeroToOne: false,
+                fee: 300
+            });
+
+            bytes memory encodedHopOneData = abi.encode(hopOneData);
+
+            hops[0] = SkipGoSwapRouter.Hop({exchangeType: 1, data: encodedHopOneData});
+
+            UniswapV3Adapter.UniswapV3Data memory hopTwoData = UniswapV3Adapter.UniswapV3Data({
+                tokenIn: usdc,
+                tokenOut: wbtc,
+                fee: 3000,
+                quoter: 0x5e55C9e631FAE526cd4B0526C4818D6e0a9eF0e3,
+                swapRouter: 0xE592427A0AEce92De3Edee1F18E0157C05861564
+            });
+
+            bytes memory encodedHopTwoData = abi.encode(hopTwoData);
+
+            hops[1] = SkipGoSwapRouter.Hop({exchangeType: 2, data: encodedHopTwoData});
+        }
+
+        uint256 amountIn = 1 ether;
+
+        address alice = makeAddr("alice");
+
+        deal(weth, alice, amountIn);
+
+        uint256 expectedAmountOut = router.getAmountOut(amountIn, hops);
+
+        vm.startPrank(alice);
+
+        IERC20(weth).approve(address(router), amountIn);
+
+        vm.expectRevert("amount out is less than amount out min");
+        router.swapExactIn(amountIn, expectedAmountOut + 1, weth, wbtc, hops);
+
+        vm.stopPrank();
+    }
+
     function test_swapExactOut() public {
         address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
         address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
@@ -143,5 +192,54 @@ contract SkipGoSwapRouterTest is Test {
         assertEq(wbtcBalanceAfter, wbtcBalanceBefore + amountOut);
 
         assertEq(amountIn, expectedAmountIn);
+    }
+
+    function test_revertSwapExactOut_WhenAmountInIsGreaterThanAmountInMax() public {
+        address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        address wbtc = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+
+        SkipGoSwapRouter.Hop[] memory hops = new SkipGoSwapRouter.Hop[](2);
+
+        {
+            UniswapV2Adapter.UniswapV2Data memory hopOneData = UniswapV2Adapter.UniswapV2Data({
+                pool: 0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc,
+                zeroToOne: false,
+                fee: 300
+            });
+
+            bytes memory encodedHopOneData = abi.encode(hopOneData);
+
+            hops[0] = SkipGoSwapRouter.Hop({exchangeType: 1, data: encodedHopOneData});
+
+            UniswapV3Adapter.UniswapV3Data memory hopTwoData = UniswapV3Adapter.UniswapV3Data({
+                tokenIn: usdc,
+                tokenOut: wbtc,
+                fee: 3000,
+                quoter: 0x5e55C9e631FAE526cd4B0526C4818D6e0a9eF0e3,
+                swapRouter: 0xE592427A0AEce92De3Edee1F18E0157C05861564
+            });
+
+            bytes memory encodedHopTwoData = abi.encode(hopTwoData);
+
+            hops[1] = SkipGoSwapRouter.Hop({exchangeType: 2, data: encodedHopTwoData});
+        }
+
+        uint256 amountOut = 100000000;
+
+        address alice = makeAddr("alice");
+
+        uint256 expectedAmountIn = router.getAmountIn(amountOut, hops);
+
+        deal(weth, alice, expectedAmountIn);
+
+        vm.startPrank(alice);
+
+        IERC20(weth).approve(address(router), expectedAmountIn);
+
+        vm.expectRevert("amount in is greater than amount in max");
+        router.swapExactOut(amountOut, expectedAmountIn - 1, weth, wbtc, hops);
+
+        vm.stopPrank();
     }
 }
